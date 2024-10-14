e559f565698cc49db7575f293fca09952535e3a6
Muneer Abrahams <audipunkwp@gmail.com>
2024-10-14 19:43:42 +0200

    update index.js

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ try {
   )`;
     document.getElementById("author").textContent = `By: Dodi Achmad`;
   }
+
+  try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin");
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    const data = await res.json();
+    document.getElementById("crypto-top").innerHTML = `
+          <img src=${data.image.small} />
+          <span>${data.name}</span>
+      `;
+    document.getElementById("crypto").innerHTML += `
+          <p>🎯: $${data.market_data.current_price.usd}</p>
+          <p>👆: $${data.market_data.high_24h.usd}</p>
+          <p>👇: $${data.market_data.low_24h.usd}</p>
+      `;
+  } catch (err) {
+    console.error(err);
+  }
+  
