edf27a3b56daf4566b91a4d50616826ad2360d66
Muneer Abrahams <audipunkwp@gmail.com>
2024-10-14 19:52:41 +0200

    update json

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 try {
     const res = await fetch(
-      "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+      "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=cyberpunk"
     );
     const data = await res.json();
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
     document.getElementById("author").textContent = `By: ${data.user.name}`;
   } catch (err) {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1628329336337-8c33a8f08ec1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTc1MDQzODZ8&ixlib=rb-4.0.3&q=80&w=1080)
   )`;
-    document.getElementById("author").textContent = `By: Dodi Achmad`;
+    document.getElementById("author").textContent = `By: Lorenzo Herrera`;
   }
-
+  
   try {
-    const res = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin");
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/pepecoin-2");
     if (!res.ok) {
       throw Error("Something went wrong");
     }
@@ -22,11 +22,10 @@ try {
           <span>${data.name}</span>
       `;
     document.getElementById("crypto").innerHTML += `
-          <p>🎯: $${data.market_data.current_price.usd}</p>
-          <p>👆: $${data.market_data.high_24h.usd}</p>
-          <p>👇: $${data.market_data.low_24h.usd}</p>
+              <p>🏷️: R ${data.market_data.current_price.zar}</p>
+              <p>📈: R ${data.market_data.high_24h.zar}</p>
+              <p>📉: R ${data.market_data.low_24h.zar}</p>
       `;
   } catch (err) {
     console.error(err);
   }
-  
